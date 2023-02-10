70d550a5ecacb3413d23422e08ff7219017db21c
alinnb <mailzyy@qq.com>
2023-02-10 15:25:16 +0800

    update

--- vim/install.sh
+++ vim/install.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-main() {
-  if [ -f ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.bak;
-  fi
-  curl https://raw.githubusercontent.com/razeencheng/dotfiles/master/macvim/vimrc -o ~/.vimrc
+if [ -f ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc.bak;
+fi
+curl https://raw.githubusercontent.com/alinnb/dotfiles/master/vim/vimrc -o ~/.vimrc
 
-  _green "Vim config is installed to ~/.vimrc."
-}
-
-main "$@"
+echo "Vim config is installed to ~/.vimrc."
